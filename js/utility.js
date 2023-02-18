bb58de4f9b2f73aafe30e32a94f81e14ba9e4e5f
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 00:02:45 +0600

    two functions twice used done

--- js/utility.js
+++ js/utility.js
@@ -1,1 +1,22 @@
-console.log("Utility is connected");
+
+function getAreaInputsValue (id){
+    const inputField = document.getElementById(id);
+    const inputFieldString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldString);
+    inputField.value = '';
+
+    return inputFieldValue;
+};
+
+
+function showAreaCalculation(calculatedAreaOf, result) {
+
+    const areaCalculationViewArea = document.getElementById("result-shower-container");
+    const resultList = document.createElement("li");
+    resultList.className = "my-3"
+    resultList.innerHTML = `
+    <span>${calculatedAreaOf}</span> <span class="mx-2 ">${result}cm<sup>2</sup></span> <button class="btn btn-primary">Covert to m<sup>2</sup></button>
+    `;
+    areaCalculationViewArea.appendChild(resultList);
+    
+};
