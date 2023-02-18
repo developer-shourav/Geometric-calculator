486aacc216a576c842b12d87c50c68e8765a7987
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 03:02:39 +0600

    first 4 cards hover random bg color added

--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,4 @@
-
+/* ======================================== */
 function getAreaInputsValue (id){
     const inputField = document.getElementById(id);
     const inputFieldString = inputField.value;
@@ -7,14 +7,14 @@ function getAreaInputsValue (id){
 
     return inputFieldValue;
 };
-
+/* ======================================== */
 function getAreaInputsFromText (id){
     const measuringInput = document.getElementById(id);
     const measuringInputString = measuringInput.innerText;
     const measuringInputValue = parseFloat(measuringInputString);
     return measuringInputValue;
 };
-
+/* ======================================== */
 function showAreaCalculation(calculatedAreaOf, result) {
 
     const areaCalculationViewArea = document.getElementById("result-shower-container");
@@ -26,3 +26,37 @@ function showAreaCalculation(calculatedAreaOf, result) {
     areaCalculationViewArea.appendChild(resultList);
     
 };
+
+
+
+
+
+function setTriangleBg(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    const triangleCard = document.getElementById("triangle-card");
+    triangleCard.style.backgroundColor = "#" + randomColor;
+}
+
+function setRectangleBg(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    const rectangleCard = document.getElementById("rectangle-card");
+    rectangleCard.style.backgroundColor = "#" + randomColor;
+}
+
+function setParallelogramBg(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    const parallelogramCard = document.getElementById("parallelogram-card");
+    parallelogramCard.style.backgroundColor = "#" + randomColor;
+}
+
+
+function setRhombusBg(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    const rhombusCard = document.getElementById("rhombus-card");
+    rhombusCard.style.backgroundColor = "#" + randomColor;
+}
+
