89e63b9a3d48b0e9560e84a9bf49cd4aba81ce48
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 00:23:26 +0600

    ractangle calculation added

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,1 +1,10 @@
-console.log("rectangle is connected");
+document.getElementById("rectangle-cal-btn").addEventListener("click", function(){
+
+    const rectanglesFirstInputValue = getAreaInputsValue("rectangle-first-input");
+    const rectanglesSecondInputValue = getAreaInputsValue("rectangle-second-input");
+
+    const areaOfRectangleFormula = rectanglesFirstInputValue * rectanglesSecondInputValue;
+    const areaOfRectangleFinalResult = areaOfRectangleFormula.toFixed(2);
+    
+    showAreaCalculation("Rectangle", areaOfRectangleFinalResult);
+})
--- js/triangle.js
+++ js/triangle.js
@@ -4,8 +4,8 @@ document.getElementById("triangle-cal-btn").addEventListener("click", function()
     const trianglesFirstInputValue = getAreaInputsValue("triangle-first-input");
     const trianglesSecondInputValue = getAreaInputsValue("triangle-second-input");
 
-    const areaOfTriangle = 0.5 * trianglesFirstInputValue * trianglesSecondInputValue;
-    const areaOfTriangleFinalResult = areaOfTriangle.toFixed(2);
+    const areaOfTriangleFormula = 0.5 * trianglesFirstInputValue * trianglesSecondInputValue;
+    const areaOfTriangleFinalResult = areaOfTriangleFormula.toFixed(2);
     
     showAreaCalculation("Triangle", areaOfTriangleFinalResult);
 })
