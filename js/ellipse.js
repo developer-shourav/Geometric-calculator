0dba9d62fc5930b2dbf2b38f0d1ef37022d313ef
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 01:48:30 +0600

    Ellipse formula completed

--- js/ellipse.js
+++ js/ellipse.js
@@ -1,1 +1,10 @@
-console.log("Ellipse is connected");
+document.getElementById("ellipse-cal-btn").addEventListener("click", function(){
+
+    const ellipseFirstInputValue = getAreaInputsFromText("ellipse-first-input");
+    const ellipseSecondInputValue = getAreaInputsFromText("ellipse-second-input");
+
+    const ellipseFormula =  3.14159 * ellipseFirstInputValue * ellipseSecondInputValue;
+    const areaOfEllipseFinalResult = ellipseFormula.toFixed(2);
+    
+    showAreaCalculation("Ellipse", areaOfEllipseFinalResult);
+})
