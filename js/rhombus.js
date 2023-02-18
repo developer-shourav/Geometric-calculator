ffdd2e8813f5382017e0855e2bd58a3e35454e1f
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 01:32:58 +0600

    Rhombus area calculation fourmula done

--- js/rhombus.js
+++ js/rhombus.js
@@ -3,7 +3,7 @@ document.getElementById("rhombus-cal-btn").addEventListener("click", function(){
     const rhombusFirstInputValue = getAreaInputsFromText("rhombus-first-input");
     const rhombusSecondInputValue = getAreaInputsFromText("rhombus-second-input");
 
-    const rhombusFormula =  rhombusFirstInputValue * rhombusSecondInputValue;
+    const rhombusFormula =  0.5 * rhombusFirstInputValue * rhombusSecondInputValue;
     const areaOfRhombusFinalResult = rhombusFormula.toFixed(2);
     
     showAreaCalculation("Rhombus", areaOfRhombusFinalResult);
