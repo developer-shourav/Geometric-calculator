57af54ff1072c31fb4e251832e2f922480f13218
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 01:39:40 +0600

    pentagon area method added

--- js/pentagon.js
+++ js/pentagon.js
@@ -1,1 +1,10 @@
-console.log("Pentagon is connected");
+document.getElementById("pentagon-cal-btn").addEventListener("click", function(){
+
+    const pentagonFirstInputValue = getAreaInputsFromText("pentagon-first-input");
+    const pentagonSecondInputValue = getAreaInputsFromText("pentagon-second-input");
+
+    const pentagonFormula =  0.5 * pentagonFirstInputValue * pentagonSecondInputValue;
+    const areaOfPentagonFinalResult = pentagonFormula.toFixed(2);
+    
+    showAreaCalculation("Pentagon", areaOfPentagonFinalResult);
+})
