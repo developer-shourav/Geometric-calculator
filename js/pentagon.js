041f5d7e178560cecfa39d60e360a387094a1afc
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 23:15:33 +0600

    pentagon js file's comments added

--- js/pentagon.js
+++ js/pentagon.js
@@ -1,10 +1,14 @@
 document.getElementById("pentagon-cal-btn").addEventListener("click", function(){
-
+/*-------pentagon first input value-----------  */
     const pentagonFirstInputValue = getAreaInputsFromText("pentagon-first-input");
+/*-------pentagon second input value-----------  */
     const pentagonSecondInputValue = getAreaInputsFromText("pentagon-second-input");
 
+/*-------pentagon area calculation method-----------  */
     const pentagonFormula =  0.5 * pentagonFirstInputValue * pentagonSecondInputValue;
     const areaOfPentagonFinalResult = pentagonFormula.toFixed(2);
-    
+
+/*-------pentagon area calculation result showing functions-----------  */
     showAreaCalculation("Pentagon", areaOfPentagonFinalResult);
 })
+
