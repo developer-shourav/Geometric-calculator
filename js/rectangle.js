2b1256eefdb8555af5bcc02636e5308cda5a604a
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 23:17:42 +0600

    rectangle js files comment added

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,10 +1,13 @@
 document.getElementById("rectangle-cal-btn").addEventListener("click", function(){
-
+/*-------rectangle first input value-----------  */
     const rectanglesFirstInputValue = getAreaInputsValue("rectangle-first-input");
+/*-------rectangle second input value-----------  */
     const rectanglesSecondInputValue = getAreaInputsValue("rectangle-second-input");
 
+/*-------rectangle area calculation method -----------  */
     const rectangleFormula = rectanglesFirstInputValue * rectanglesSecondInputValue;
     const areaOfRectangleFinalResult = rectangleFormula.toFixed(2);
-    
+
+/*-------rectangle area calculation result showing function -----------  */
     showAreaCalculation("Rectangle", areaOfRectangleFinalResult);
 })
