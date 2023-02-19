a34193bbe2fc299c502d5ba8a5e30e42680faab1
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 23:13:35 +0600

    ellipse and parallelogram js file updated

--- js/ellipse.js
+++ js/ellipse.js
@@ -1,10 +1,11 @@
 document.getElementById("ellipse-cal-btn").addEventListener("click", function(){
-
+/*-------ellipse first input value-----------  */
     const ellipseFirstInputValue = getAreaInputsFromText("ellipse-first-input");
+/*-------ellipse second input value-----------  */
     const ellipseSecondInputValue = getAreaInputsFromText("ellipse-second-input");
-
+/*-------ellipse area calculation-----------  */
     const ellipseFormula =  3.14159 * ellipseFirstInputValue * ellipseSecondInputValue;
     const areaOfEllipseFinalResult = ellipseFormula.toFixed(2);
-    
+/*-------ellipse area result showing function-----------  */
     showAreaCalculation("Ellipse", areaOfEllipseFinalResult);
 })
--- js/parallelogram.js
+++ js/parallelogram.js
@@ -1,10 +1,13 @@
 document.getElementById("parallelo-cal-btn").addEventListener("click", function(){
-
+/*-------parallelogram first input value-----------  */
     const parallelosFirstInputValue = getAreaInputsFromText("parallelo-first-input");
+/*-------parallelogram second input value-----------  */
     const parallelosSecondInputValue = getAreaInputsFromText("parallelo-second-input");
 
+/*-------parallelogram Area calculation method -----------  */
     const paralleloFormula =  parallelosFirstInputValue * parallelosSecondInputValue;
     const areaOfParalleloFinalResult = paralleloFormula.toFixed(2);
-    
+
+/*-------parallelogram Area calculation result showing function -----------  */
     showAreaCalculation("Parallelogram", areaOfParalleloFinalResult);
 })
