835e003ccd40779f10fcd33be0c29bb3f8ce4acc
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 23:21:40 +0600

    triange files usefull comment added

--- js/triangle.js
+++ js/triangle.js
@@ -1,15 +1,16 @@
 
 document.getElementById("triangle-cal-btn").addEventListener("click", function(){
+/*-------triangle first input value-----------  */
+    const trianglesFirstInputValue = getAreaInputsValue("triangle-first-input");
+/*-------triangle second input value-----------  */
+    const trianglesSecondInputValue = getAreaInputsValue("triangle-second-input");
 
-    var trianglesFirstInputValue = getAreaInputsValue("triangle-first-input");
-    var trianglesSecondInputValue = getAreaInputsValue("triangle-second-input");
-
+/*-------triangle area calculation Method-----------  */
         const triangleFormula = 0.5 * trianglesFirstInputValue * trianglesSecondInputValue;
         const areaOfTriangleFinalResult = triangleFormula.toFixed(2);
+
+/*-------triangle area calculation result showing functions-----------  */
         showAreaCalculation("Triangle", areaOfTriangleFinalResult);
 
-
-    
- 
 })
 
