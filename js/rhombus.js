4ce5108f2cd483786a4d22dae8ad7ded4ecddc6f
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 23:19:43 +0600

    rhombus's files comments added

--- js/rhombus.js
+++ js/rhombus.js
@@ -1,10 +1,13 @@
 document.getElementById("rhombus-cal-btn").addEventListener("click", function(){
-
+/*-------rhombus first input value-----------  */
     const rhombusFirstInputValue = getAreaInputsFromText("rhombus-first-input");
+/*-------rhombus second input value-----------  */
     const rhombusSecondInputValue = getAreaInputsFromText("rhombus-second-input");
 
+/*-------rhombus are calculation method -----------  */
     const rhombusFormula =  0.5 * rhombusFirstInputValue * rhombusSecondInputValue;
     const areaOfRhombusFinalResult = rhombusFormula.toFixed(2);
-    
+
+/*-------rhombus are calculation result  showing function-----------  */
     showAreaCalculation("Rhombus", areaOfRhombusFinalResult);
 })
