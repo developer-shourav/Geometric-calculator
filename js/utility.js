317374d494d037ec685b62e2d64e64c8d7ad6c87
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-19 22:41:25 +0600

    blog page toggle btn added and bootStrap script added

--- js/utility.js
+++ js/utility.js
@@ -96,3 +96,13 @@ function setEllipseBg(){
     ellipseCard.style.backgroundColor = "#" + randomColor;
 }
 
+
+document.getElementById("blog-btn").addEventListener("click", function(){
+    window.location.href = "blog.html";
+})
+
+document.getElementById("home-btn").addEventListener("click", function(){
+    window.location.href = "index.html";
+})
+
+
